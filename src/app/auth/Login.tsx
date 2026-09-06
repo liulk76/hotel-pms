9cfff035c74027e06f9e55954801208398d3b146
liulk76 <llk0707@outlook.com>
2026-09-06 16:03:36 +0800

    fix: login

--- src/app/auth/Login.tsx
+++ src/app/auth/Login.tsx
@@ -1,28 +1,39 @@
 import logo from "@/assets/logo.svg"
 import { LoginForm } from "@/components/login-form"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <img src={logo} alt="住客邦" className="size-6 rounded-md" />
-            住客邦 - 酒店云数平台
-          </a>
-        </div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <LoginForm />
-          </div>
-        </div>
-      </div>
       <div className="relative hidden bg-muted lg:block">
         <img
           src="/hotel-bg.jpg"
           alt="酒店大堂"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.6]"
         />
+      </div>
+      <div className="flex gap-4 py-10">
+        <div className="flex flex-1 flex-col items-center justify-center">
+          <div className="flex justify-center gap-2 md:justify-center mb-10">
+            <a href="#" className="flex items-center gap-2 font-medium">
+              <img src={logo} alt="住客邦" className="size-6 rounded-md" />
+              住客邦 - 酒店云数平台
+            </a>
+          </div>
+          <Card className="w-full max-w-sm px-2 py-5 shadow-2xl">
+            <CardContent>
+              <LoginForm />
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   )
